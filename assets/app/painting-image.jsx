0e103563fd23cf49f125014a221838cc43bbb317
Bernard Bolter <b@bernardbolter.com>
2015-12-12 13:48:26 -0800

    added lazysizes to images

--- assets/app/painting-image.jsx
+++ assets/app/painting-image.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var PictureFill = require('picturefill');
+var LazySizes = require('lazysizes');
 
 module.exports = React.createClass({
 	render: function() {
@@ -9,7 +11,11 @@ module.exports = React.createClass({
 					</section>
 				},
 				paintingImageRender: function() {
-						var link = './img/paintings/' + this.props.web_title + '/' + this.props.web_title + 'x1200.jpg';
-					return <img src={link} />
+						var dataSize = 'auto'
+						var dataSrc = './img/paintings/' + this.props.web_title + '/' + this.props.web_title + 'x400.jpg 280w, ./img/paintings/' + this.props.web_title + '/' + this.props.web_title + 'x800.jpg 801w, ./img/paintings/' + this.props.web_title + '/' + this.props.web_title + 'x1200.jpg 1201w, ./img/paintings/' + this.props.web_title + '/' + this.props.web_title + 'x1600.jpg 1601w';
+						var lazyClass = 'lazyload'
+						var imageAlt = this.props.title + ' ' + this.props.photo_year
+
+					return <img data-size={dataSize} data-srcset={dataSrc} className={lazyClass} alt={imageAlt} />
 				}
 });
